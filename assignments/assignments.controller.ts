270a199a94f2e76cf89b7d83a96caabd1fb9e539
jenny-caasi <caasijenny028@gmail.com>
2024-11-09 22:06:19 +0800

    Completed Assignment 2: Prime Number Checker

--- assignments/assignments.controller.ts
+++ assignments/assignments.controller.ts
@@ -8,6 +8,24 @@ export class AssignmentsController {
     return { sequence };
   }
 
+  @Get('prime/:number')
+  checkPrime(@Param('number') number: number): { isPrime: boolean } {
+    const isPrime = this.isPrime(Number(number));
+    return { isPrime };
+  }
+
+  private isPrime(number: number): boolean {
+    if (number <= 1) return false;
+    if (number <= 3) return true;
+    if (number % 2 === 0 || number % 3 === 0) return false;
+
+    for (let i = 5; i * i <= number; i += 6) {
+      if (number % i === 0 || number % (i + 2) === 0) return false;
+    }
+
+    return true;
+  }
+
   private generateFibonacci(n: number): number[] {
     if (n <= 0) return [];
     if (n === 1) return [0];
