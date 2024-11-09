22cb30b805da186e329dbcd647f0a9efe11b95be
jenny-caasi <caasijenny028@gmail.com>
2024-11-09 22:06:53 +0800

    Completed Assignment 3: Factorial Calculator

--- assignments/assignments.controller.ts
+++ assignments/assignments.controller.ts
@@ -14,6 +14,25 @@ export class AssignmentsController {
     return { isPrime };
   }
 
+  @Get('factorial/:number')
+  calculateFactorial(@Param('number') number: number): { factorial: number } {
+    const factorial = this.getFactorial(Number(number));
+    return { factorial };
+  }
+
+  private getFactorial(number: number): number {
+    if (number < 0) {
+      throw new Error('Factorial is not defined for negative numbers');
+    }
+    if (number === 0 || number === 1) return 1;
+
+    let result = 1;
+    for (let i = 2; i <= number; i++) {
+      result *= i;
+    }
+    return result;
+  }
+
   private isPrime(number: number): boolean {
     if (number <= 1) return false;
     if (number <= 3) return true;
